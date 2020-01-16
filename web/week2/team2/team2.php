b73efca9556b853951bdb9898d8ba58b850e1b9d
Conrad Kramer <Conradakramer@gmail.com>
2020-01-16 16:14:51 -0700

    team assignment

--- web/week2/team2/team2.php
+++ web/week2/team2/team2.php
@@ -12,11 +12,9 @@
     <?php session_start(); ?>
     <h1>PHP Team Activity</h1>
     <!-- loop -->
-    <?php for ($i = 0; $i < 10; $i++) { ?>
+    <?php for ($i = 0; $i < 10; $i++) { ?>
 
-        <div id=“<?php echo $i; ?>” class=“numberedDivs”
-
-            <?php if($i % 2 == 0){echo “style=‘color:red’”;} ?>>
+        <div id="<?php echo $i; ?>" class="numberedDivs" <?php if($i % 2 == 0){echo "style=‘color:red’";} ?>>
 
             This is div #<?php echo $i; ?></div>
 
