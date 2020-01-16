a8ac1a7233d5ab358e594ee37e729f21b39f6c14
Conrad Kramer <Conradakramer@gmail.com>
2020-01-16 16:04:39 -0700

    team assignment

--- web/week2/team2/team2.php
+++ web/week2/team2/team2.php
@@ -3,20 +3,24 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Yay team</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="team2.css">
     </head>
     <body>
-        <?php for ($i=0; $i < 11; $i++) { 
-          $s += '<div';
-          if($i % 2 == 0){
-            $s += ' class="even"';
-          }
-          $s += '>This is div #' + $i + '</div>';
-        } ?>
-        <?php echo $s; ?>
+    <?php session_start(); ?>
+    <h1>PHP Team Activity</h1>
+    <!-- loop -->
+    <?php for ($i = 0; $i < 10; $i++) { ?>
+
+        <div id=“<?php echo $i; ?>” class=“numberedDivs”
+
+            <?php if($i % 2 == 0){echo “style=‘color:red’”;} ?>>
+
+            This is div #<?php echo $i; ?></div>
+
+         <?php } ?>
         <script src="" async defer></script>
     </body>
 </html>
@@ -26,15 +30,3 @@
 <!-- Generate 10 divs with a Loop -->
 <!-- Even Number make different font color (add a class the even numbered divs) -->
 <!-- For Loop -->
-
-
-<!-- loop -->
-<?php for ($i = 0; $i < 10; $i++) { ?>
-
-	<div id=“<?php echo $i; ?>” class=“numberedDivs”
-
-		<?php if($i % 2 == 0){echo “style=‘color:red’”; } ?>
-
-		This is div #<?php echo $i; ?></div>
-
-<?php> } <?>
