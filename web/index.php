e4bfe188a12b1c542512c272562fc206dc44150d
Conrad Kramer <Conradakramer@gmail.com>
2020-01-16 14:38:01 -0700

    adding assignment page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Assignments</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="container">
+        <ul>
+          <<li><a href="week1/hello.html">week1 - Hello World </a></li>
+          <li><a href="week2/assignment/about.html">week2 - Home Page </a></li>
+        </ul>
+    </div>
+    <script src="" async defer></script>
+</body>
+
+</html>
+
+
+
 <?php
 
-  phpinfo();
+
 
 ?>
