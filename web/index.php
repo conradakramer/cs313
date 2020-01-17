70dd9fa4687396ff0e7cf38a3e9c9d7d3c467880
Conrad Kramer <Conradakramer@gmail.com>
2020-01-17 09:14:53 -0700

    homepage

--- web/index.php
+++ web/index.php
@@ -12,7 +12,10 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-
+    <div class="container-sm">100% wide until small breakpoint</div>
+    <div class="container-md">100% wide until medium breakpoint</div>
+    <div class="container-lg">100% wide until large breakpoint</div>
+    <div class="container-xl">100% wide until extra large breakpoint</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
