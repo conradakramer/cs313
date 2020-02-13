63085646c2bbcb454b166cdcfdbb2dc405dec5d4
Conrad Kramer <Conradakramer@gmail.com>
2020-02-13 16:33:06 -0700

    week 6

--- web/week6/assignment/main.php
+++ web/week6/assignment/main.php
@@ -9,7 +9,7 @@
     <form action="register.php" method="POST">
         <div class="form-group" >
             <label for="exampleInputEmail1">Username</label>
-            <input type="" class="form-control" id="username" aria-describedby="emailHelp">
+            <input type="text" class="form-control" id="username" aria-describedby="emailHelp">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
