0d84448c26e9b19c96f725eacadf4512e77dab1e
Conrad Kramer <Conradakramer@gmail.com>
2020-02-13 15:36:31 -0700

    week 6 tuesday

--- web/week6/assignment/main.php
+++ web/week6/assignment/main.php
@@ -1,10 +1,23 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    </head>
+    <?php
 	require("dbConnect.php");
 	$db = get_db();
 ?>
-<body>
-   <div class="container" style="margin-top:50px;">
+    <body>
+        
+    <div class="container" style="margin-top:50px;">
       <form action="insert.php" method="POST">
          <div class="form-row">
             <div class="col">
@@ -15,6 +28,10 @@
             </div>
             <div class="col">
                   <select id="inputFood" class="form-control" name="food">
+                     
+                     
+                     
+                     
                      <?php
                        $statement = $db->prepare("SELECT * FROM w6_food");
                        $statement->execute();
@@ -33,22 +50,6 @@
          </div>
       </form>
    </div>
-</body>
-
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        
-        
         <script src="" async defer></script>
     </body>
 </html>
