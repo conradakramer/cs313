855507a102b6f9506ee1fc9a464b95dd02b00332
conradakramer <conradakramer@gmail.com>
2020-09-29 09:50:57 -0500

    Update assignments.php

--- web/assignments.php
+++ web/assignments.php
@@ -1,35 +1,38 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Assignments</title>
-  </head>
-  <body>
-    <!--<h1>Hello, world!</h1>-->
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <title>Assignments</title>
+</head>
 
-    <H1>Assignments</H1>
-    <div class="card mb-4 shadow-sm">
+<body class="m-auto pt-3">
+  <div class="card m-auto" style="width: 55rem;">
+    <div class="card-header">
+      <h2>Assignments</h2>
       <div class="card-body">
-        <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='week1/hello.html'">Week 01 - Hello World</button>
-      </div>
-    </div>
-    <div class="card mb-4 shadow-sm">
-      <div class="card-body">
-        <button type="button" class="btn btn-lg btn-block btn-primary"onclick="location.href='week2/assignment/assign02.html'">Week 02 - About Me</button>
-      </div>
-    </div>
-    <div class="card mb-4 shadow-sm">
-      <div class="card-body">
-        <button type="button" class="btn btn-lg btn-block btn-primary"onclick="location.href='week3/team/teamActivity1.php'">Week 03 - Team Activity</button>
-      </div>
-    </div>
-    <!-- <div class="card mb-4 shadow-sm">
+        <div class="container">
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='week1/hello.html'">Week 01 - Hello World</button>
+            </div>
+          </div>
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='week2/assignment/assign02.html'">Week 02 - About Me</button>
+            </div>
+          </div>
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='week3/team/teamActivity1.php'">Week 03 - Team Activity</button>
+            </div>
+          </div>
+          <!-- <div class="card mb-4 shadow-sm">
       <div class="card-body">
         <button type="button" class="btn btn-lg btn-block btn-primary"onclick="location.href='week3/assignment/browse.php'">Week 03 - Shoping</button>
       </div>
@@ -50,12 +53,17 @@
       </div>
     </div> -->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+          <!-- Optional JavaScript -->
+          <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+          <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
 
 
