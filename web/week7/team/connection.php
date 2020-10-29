39d5b80a1e89b30abcb56a15354c85d86452b3b1
conradakramer <conradakramer@gmail.com>
2020-10-28 20:14:46 -0500

    agian

--- web/week7/team/connection.php
+++ web/week7/team/connection.php
@@ -1,12 +1,8 @@
 <?php
-  $connection = "dbname=d21mudpnktt1pq host=ec2-34-233-43-35.compute-1.amazonaws.com port=5432 user=pbwhjtpmsxjqrm password=389e50cbd7adabb3d3d594461762a15661f1bae8fa1fe65dbfb3983a5d07e2eb sslmode=require";
-  $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-  // Create the actual connection object and assign it to a variable
-  try {
-    $link = new PDO($connection, $username, $password, $options);
-    return $link;
-  } catch (PDOException $e) {
-    //include "500.php";
-    echo "Error";
-    exit;
+ try {
+    $connection = "dbname=d21mudpnktt1pq host=ec2-34-233-43-35.compute-1.amazonaws.com port=5432 user=pbwhjtpmsxjqrm password=389e50cbd7adabb3d3d594461762a15661f1bae8fa1fe65dbfb3983a5d07e2eb sslmode=require";
+    $connect = new PDO("pgsql:$connection");
+  }catch (PDOException $e) {
+  echo "Error : " . $e->getMessage() . "<br/>";
   }
+?>
