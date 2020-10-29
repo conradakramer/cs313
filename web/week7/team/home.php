f3d9ad5256f25adb1fc7ae8bc3872db7c2fdc822
conradakramer <conradakramer@gmail.com>
2020-10-28 19:39:38 -0500

    push

--- web/week7/team/home.php
+++ web/week7/team/home.php
@@ -1,11 +1,4 @@
-<!-- <?php
-session_start();
-include 'connection.php';
-if (isset($_SESSION['logged'])) {
-    $username = $_SESSION['userData']['username'];
-}
 
-?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
