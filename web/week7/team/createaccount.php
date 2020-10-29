0f8c87c4e76fb1e7741be3b50d839cdb7bf7b1cb
conradakramer <conradakramer@gmail.com>
2020-10-28 20:17:03 -0500

    connect

--- web/week7/team/createaccount.php
+++ web/week7/team/createaccount.php
@@ -4,7 +4,6 @@ $username = $_POST['username'];
 $pass = $_POST['pass'];
 $pass2 = $_POST['pass2'];
 
-error_log("break 1");
 $checkPassword = checkPassword($pass);
 $checkPassword2 = checkPassword($pass2);
 
@@ -15,22 +14,19 @@ if (!isset($username) || $username == ""
 	header("Location: signup.php");
 	die(); // we always include a die after redirects.
 }
-error_log("break 2");
 
 if ($pass != $pass2) {
     header("Location: signup.php");
 	die(); // we always include a die after redirects.
 }
-error_log("break 3");
 $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
 require("connection.php");
 $query = 'INSERT INTO "login" (username, pass) VALUES(:username, :pass)';
-$stmt = $db->prepare($query);
+$stmt = $connect->prepare($query);
 $stmt->bindValue(":username", $username, PDO::PARAM_STR);
 $stmt->bindValue(":pass", $hashedPass, PDO::PARAM_STR);
 $stmt->execute();
 
-error_log("break 4");
 header("Location: signin.php");
 die();
 
