a3ba29107e32ab9e22a9022f8c598c3a606876c8
conradakramer <conradakramer@gmail.com>
2020-10-28 21:36:28 -0500

    session

--- web/week7/team/home.php
+++ web/week7/team/home.php
@@ -4,15 +4,12 @@ session_start();
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
 }
-
-?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome, <?php echo $username ?></title>
-</head>
+if ($_SESSION['username'] == ''){
+    header('Location:signin.php');
+    die();
+}
+?>
+<html>
 <body>
     <h1>Welcome, <?php echo $username ?></h1>
     <a href="signout.php">Logout</a>
