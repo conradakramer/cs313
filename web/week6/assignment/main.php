386736540382f1bf9d75e11c94a9d4a082a444c3
conradakramer <conradakramer@gmail.com>
2020-11-01 08:37:22 -0600

    register button

--- web/week6/assignment/main.php
+++ web/week6/assignment/main.php
@@ -141,8 +141,8 @@ else
     <input type="text" id="username" class="form-control" placeholder="username" name="username" required="" autofocus="">
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="password" class="form-control" placeholder="Password" name="password" required="">
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    <a href="register.php"><button class="btn btn-lg btn-primary btn-block" type="submit">Register</button></a>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button><br>
+    <a href="register.php"><button class="btn btn-lg btn-primary btn-block" type="register">Register</button></a>
     <p class="mt-5 mb-3 text-muted">© 2020</p>
   </form>
     
